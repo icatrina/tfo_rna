2125e0e23dcece84d6e13ad57190a5662b786149
Irina E. Catrina <iecatrina@gmail.com>
2022-10-04 15:01:47 -0400

    Update tfofinder_win.py

--- tfofinder_win.py
+++ tfofinder_win.py
@@ -47,7 +47,7 @@ def parallel_complement(seq): #generate RNA complement
     return seq.translate(basecomplement)[::1]
 
 def probeLength(probe): #input desired probe length; limited to range[18,26]
-    if probe <6 or probe >12:
+    if probe <4 or probe >12:
         print('The value you entered is incorrect!')
         sys.exit('Try again!')
     else:
@@ -94,9 +94,9 @@ if __name__ == "__main__":
 
     while True: #probe length?
         try:
-            probe=int(input("Enter the length of TFO probe; a number between 6 and 12: "))
+            probe=int(input("Enter the length of TFO probe; a number between 4 and 12: "))
         except:
-            print('You must type a number between 6 and 12, try again:')
+            print('You must type a number between 4 and 12, try again:')
             continue
 
         else:
